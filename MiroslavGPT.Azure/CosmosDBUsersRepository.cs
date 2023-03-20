b5a0a98860fe902580f3a858dafa57d267f1e5e9
Miroslav Bogatyrev <mistermiros@gmail.com>
2023-03-21 00:32:17 +0100

    project and classes

---
+++ MiroslavGPT.Azure/CosmosDBUsersRepository.cs
@@ -0,0 +1,52 @@
+﻿using MiroslavGPT.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+
+namespace MiroslavGPT.Azure
+{
+    public class CosmosDBUsersRepository : IUsersRepository
+    {
+        private readonly Container _container;
+
+        public CosmosDBUsersRepository(string endpointUri, string primaryKey, string databaseId, string containerId)
+        {
+            var client = new CosmosClient(endpointUri, primaryKey);
+            var database = client.GetDatabase(databaseId);
+            _container = database.GetContainer(containerId);
+        }
+
+        public async Task AuthorizeUserAsync(long chatId)
+        {
+            var item = new UserDocument
+            {
+                ChatId = chatId,
+                Authorized = true
+            };
+
+            await _container.CreateItemAsync(item);
+        }
+
+        public async Task<bool> IsAuthorizedAsync(long chatId)
+        {
+            var query = new QueryDefinition("SELECT * FROM c WHERE c.ChatId = @chatId AND c.Authorized = true")
+                .WithParameter("@chatId", chatId);
+
+            var iterator = _container.GetItemQueryIterator<UserDocument>(query);
+
+            while (iterator.HasMoreResults)
+            {
+                var response = await iterator.ReadNextAsync();
+                foreach (var item in response)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ MiroslavGPT.Azure/TelegramWebhookFunction.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using MiroslavGPT.Domain;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+
+namespace MiroslavGPT.Azure
+{
+    public class TelegramWebhookFunction
+    {
+        private readonly TelegramMessageHandler _telegramMessageHandler;
+
+        public TelegramWebhookFunction()
+        {
+            string endpointUri = Environment.GetEnvironmentVariable("COSMOS_ENDPOINT_URI");
+            string primaryKey = Environment.GetEnvironmentVariable("COSMOS_PRIMARY_KEY");
+            string databaseId = Environment.GetEnvironmentVariable("COSMOS_DATABASE_ID");
+            string containerId = Environment.GetEnvironmentVariable("COSMOS_CONTAINER_ID");
+            string secretKey = Environment.GetEnvironmentVariable("SECRET_KEY");
+            string openAiApiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY");
+            string telegramBotToken = Environment.GetEnvironmentVariable("TELEGRAM_BOT_TOKEN");
+            string botUsername = Environment.GetEnvironmentVariable("TELEGRAM_BOT_USERNAME");
+            int maxTokens = int.Parse(Environment.GetEnvironmentVariable("MAX_TOKENS") ?? "100");
+
+            IUsersRepository usersRepository = new CosmosDBUsersRepository(endpointUri, primaryKey, databaseId, containerId);
+            ChatGPTBot chatGPTBot = new ChatGPTBot(secretKey, usersRepository, openAiApiKey, maxTokens);
+
+            _telegramMessageHandler = new TelegramMessageHandler(chatGPTBot, telegramBotToken, botUsername);
+        }
+
+        public async Task<IActionResult> Run(HttpRequest req, ILogger log)
+        {
+            try
+            {
+                var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                var update = JsonConvert.DeserializeObject<Update>(requestBody);
+                await _telegramMessageHandler.ProcessUpdateAsync(update);
+
+                return new OkResult();
+            }
+            catch (Exception ex)
+            {
+                log.LogError($"Error processing webhook request: {ex.Message}");
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
