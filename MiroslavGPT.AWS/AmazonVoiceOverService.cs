a95197bbd44915196f9ece8504efebdc22b0f67b
Miroslav Bogatyrev <mistermiros@gmail.com>
2023-03-28 00:41:08 +0200

    aws vo

---
+++ MiroslavGPT.AWS/AmazonVoiceOverService.cs
@@ -0,0 +1,12 @@
+﻿using MiroslavGPT.Domain.Interfaces;
+
+namespace MiroslavGPT.AWS
+{
+    public class AmazonVoiceOverService : IVoiceOverService
+    {
+        public MemoryStream VoiceOver(string text)
+        {
+            return null;
+        }
+    }
+}
