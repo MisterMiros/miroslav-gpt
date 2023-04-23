cd85a44407801b67c465b435a972c9721fe73714
Miroslav Bogatyrev <mistermiros@gmail.com>
2023-04-23 22:36:00 +0200

    thread repository

---
+++ MiroslavGPT.Domain/Models/ThreadMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace MiroslavGPT.Domain.Models
+{
+    public class ThreadMessage
+    {
+        public long? MessageId { get; set; }
+        public long? UserId { get; set; }
+        public string Message { get; set; }
+    }
+}
