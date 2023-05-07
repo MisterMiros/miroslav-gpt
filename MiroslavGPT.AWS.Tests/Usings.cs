021fb5be9e609f35ad1695bae6af47f2fffd605c
Miroslav Bogatyrev <mistermiros@gmail.com>
2023-05-07 13:08:54 +0200

    removed aws version

--- MiroslavGPT.AWS.Tests/Usings.cs
+++
@@ -1,6 +0,0 @@
-global using NUnit.Framework;
-global using FluentAssertions;
-global using AutoFixture;
-global using AutoFixture.AutoMoq;
-global using AutoFixture.NUnit3;
-global using Moq;
--- MiroslavGPT.AWS/Settings/IThreadSettings.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MiroslavGPT.AWS.Settings;
-
-public interface IThreadSettings
-{
-    public string ThreadTableName { get; set; }
-    public int ThreadLengthLimit { get; set; }
-}
